3204902831485114dc43001632b8a4f3a82fd5de
Abhishek Kumar <166218123+Abhishek-Jha0@users.noreply.github.com>
2025-03-19 00:05:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -95,4 +95,4 @@ def index():
     return render_template('index.html', predictions=None, symptoms=l1)  # Pass symptoms to the template
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000, debug=True)
